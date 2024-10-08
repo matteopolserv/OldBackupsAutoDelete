d72a448fbd214e242a5b08f39bdb8c28bc6aea5d
matteopolserv <mateuszchejzdral@gmail.com>
2024-10-08 12:19:09 +0200

    zmiany20241008

---
+++ OldBackupsAutoDelete/Logs/Logger.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OldBackupsAutoDelete.Logs
+{
+    public class Logger
+    {
+        public async Task SaveInformationToLog(string information)
+        {
+            try
+            {
+                string directoryName =  "logs";
+                if (!Directory.Exists(directoryName))
+                {
+                    Directory.CreateDirectory(directoryName);
+                }
+                string fileName = Path.Combine(directoryName, $"{DateTime.Now:yyyy-MM-dd}-log.txt");
+                using StreamWriter sw = new(fileName, true);
+                await sw.WriteLineAsync($"{DateTime.Now} {information}");
+                sw.Close();
+            }
+            catch (Exception ex)
+            {
+                await Task.Delay(1000);
+                await SaveInformationToLog(information);
+            }
+        }
+    }
+}
